ccb40e984435bfbb2e85709f17078e0ba4f0714a
Ugur Coskun <ugurcoskun0804@gmail.com>
2025-04-29 19:10:34 +0300

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "4c4ca6bb233ef9ca3b5172891aacb992";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
